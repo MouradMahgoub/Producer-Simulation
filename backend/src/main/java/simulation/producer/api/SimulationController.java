b69c8e7f86540656f04198d18fc5f5c735a9b8cc
MouradMahgoub <mouradmahgoub710@gmail.com>
2024-01-23 15:58:17 +0200

    Add newSimulation endpoint and remove commented code

--- backend/src/main/java/simulation/producer/api/SimulationController.java
+++ backend/src/main/java/simulation/producer/api/SimulationController.java
@@ -15,22 +15,23 @@ public class SimulationController {
         SimulationManager.getInstance().start();
     }
 
+    @MessageMapping("/newSimulation")
+    public void handleNewSimulation(String message) {
+        SimulationManager.getInstance().newSimulation();
+    }
+
     @MessageMapping("/pause")
     public void handlePause(String message) {
         SimulationManager.getInstance().pause();
     }
 
     @MessageMapping("/resume")
-    // @SendTo("/topic/updates")
     public void handleResume(String message) {
         SimulationManager.getInstance().resume();
-        // return "Simulation paused!";
     }
 
     @MessageMapping("/replay")
-    // @SendTo("/topic/updates")
     public void handleReply(String message) {
         SimulationManager.getInstance().replay();
-        // return "Simulation paused!";
     }
 }
